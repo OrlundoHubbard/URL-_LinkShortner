b1197bab9e923e484d6c6855f832350dd3712b54
Orlundo Hubbard <orlundokhubbard@gmail.com>
2022-12-01 13:09:30 -0600

    more js stuff

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const formSubmit = document.getElementById("linkForm");
+
+const getShortenLink = (link) => {
+  const shortenLink = document.getElementById("shortenLink");
+  shortenLink.innerHTML = link;
+};
+
+const sendData = () => {
+  const url = document.getElementById("linkInput").value;
+  const TOKEN = process.env.linkTOKEN;
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  };
+};
